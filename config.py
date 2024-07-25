ce3d4166b128e3f1327578a7bcff45c4e0ece6e2
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-07-25 15:01:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ if os.path.exists(".env"):
 # Necessary Vars
 API_ID = 25742938
 API_HASH = "b35b715fe8dc0a58e8048988286fc5b6"
-SESSION = os.getenv("SESSION", "BAGIzloAuVqc9pUpiCUzz-RhxrSuiTJGdFo4S0qAmxw3JclCDW3Caq4omKt2-cmlvRGZsthmvlaACB5ULFMlqQdpmI4CGHBygYHNnZmdqSEEpFjm8GUtQ3yOLXCJlHdINlP05dwGH1EDzWnTnDFhza_-lGBqa96Vvj1ggwZMHX9ea4M_U7qoKuNqc8N-vM6YVIREZTEAVSB3KZQwTmtS-m7zOwWg-AhV0hE1vNd9sTSxgrIK0Uilv03xoYtRAXRhd4VF6enxkQcfl4crgTrJO6ttUr2SjF5MsYYlfKVCshh1VS9ajcRTavAb1RbEcr62aeSB1w6z9bfVuDRLkRXxLECdmVmDxgAAAAF6nXj9AA")
+SESSION = os.getenv("SESSION")
 HNDLR = os.getenv("HNDLR", "!")
 GROUP_MODE = os.getenv("GROUP_MODE", "True")
 
